de0af7631e2b075028855ba65828042c443ca527
Vitor Bertolazo <VitorB@adherium.com>
2026-07-02 15:37:18 -0300

    Voltando codigo

--- script.js
+++ script.js
@@ -207,6 +207,11 @@ function renderTotal() {
 
 carregarDados();
 
+setInterval(
+    carregarDados,
+    3000000
+);
+
 document
     .getElementById('copiar-pix')
     .addEventListener('click', async () => {
